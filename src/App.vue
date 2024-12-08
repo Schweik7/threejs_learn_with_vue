df3b6821dbce70d4c9fa05a99deccf00c674d47e
Schweik7 <weibidewo0@gmail.com>
2024-12-08 11:42:02 +0800

    first add

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { OrbitControls } from 'three-stdlib';
+import { Scene,BoxGeometry } from 'three';
+
+const scene = new Scene();
+const geometry = new BoxGeometry(1, 1, 1);
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
